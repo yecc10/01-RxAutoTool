a47f023446539f517fa75e33118cf20c150dbdc2
Administrator <Administrator@DESKTOP-5HK1A1I>
2020-05-16 16:25:15 +0800

    aaa

--- AutoDeskLine_ToPlant/AutoDeskLine_ToPlant/CatiaQuickTool.Designer.cs
+++ AutoDeskLine_ToPlant/AutoDeskLine_ToPlant/CatiaQuickTool.Designer.cs
@@ -30,15 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CatiaQuickTool));
             this.TryRead = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.SDelmia = new System.Windows.Forms.RadioButton();
-            this.SCATIA = new System.Windows.Forms.RadioButton();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TryRead
             // 
-            this.TryRead.Location = new System.Drawing.Point(425, 205);
+            this.TryRead.Location = new System.Drawing.Point(12, 12);
             this.TryRead.Name = "TryRead";
             this.TryRead.Size = new System.Drawing.Size(118, 44);
             this.TryRead.TabIndex = 0;
@@ -46,53 +42,17 @@
             this.TryRead.UseVisualStyleBackColor = true;
             this.TryRead.Click += new System.EventHandler(this.TryRead_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.SDelmia);
-            this.groupBox1.Controls.Add(this.SCATIA);
-            this.groupBox1.Location = new System.Drawing.Point(12, 205);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(139, 45);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "目标设定";
-            // 
-            // SDelmia
-            // 
-            this.SDelmia.AutoSize = true;
-            this.SDelmia.Checked = true;
-            this.SDelmia.Location = new System.Drawing.Point(65, 20);
-            this.SDelmia.Name = "SDelmia";
-            this.SDelmia.Size = new System.Drawing.Size(59, 16);
-            this.SDelmia.TabIndex = 1;
-            this.SDelmia.TabStop = true;
-            this.SDelmia.Text = "DELMIA";
-            this.SDelmia.UseVisualStyleBackColor = true;
-            // 
-            // SCATIA
-            // 
-            this.SCATIA.AutoSize = true;
-            this.SCATIA.Location = new System.Drawing.Point(6, 20);
-            this.SCATIA.Name = "SCATIA";
-            this.SCATIA.Size = new System.Drawing.Size(53, 16);
-            this.SCATIA.TabIndex = 0;
-            this.SCATIA.Text = "CATIA";
-            this.SCATIA.UseVisualStyleBackColor = true;
-            // 
             // CatiaQuickTool
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(555, 261);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(189, 68);
             this.Controls.Add(this.TryRead);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CatiaQuickTool";
             this.Text = "CatiaQuickTool";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CatiaQuickTool_FormClosed);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -100,8 +60,5 @@
         #endregion
 
         private System.Windows.Forms.Button TryRead;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton SDelmia;
-        private System.Windows.Forms.RadioButton SCATIA;
     }
 }
--- AutoDeskLine_ToPlant/AutoDeskLine_ToPlant/CatiaQuickTool.cs
+++ AutoDeskLine_ToPlant/AutoDeskLine_ToPlant/CatiaQuickTool.cs
@@ -20,7 +20,8 @@ using PARTITF;
 using ProductStructureTypeLib;
 using SPATypeLib;
 using NavigatorTypeLib;
-
+using KnowledgewareTypeLib;
+using HybridShapeTypeLib;
 
 namespace AutoDeskLine_ToPlant
 {
@@ -37,16 +38,59 @@ namespace AutoDeskLine_ToPlant
         {
             try
             {
-               CatApplication = (INFITF.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Catia.Application");
+                CatApplication = (INFITF.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Catia.Application");
             }
             catch (Exception)
             {
-
                 throw;
             }
+            CatApplication.set_Caption("正在运行瑞祥快速建模工具！");
             // 获取当前活动ProductDocument
-            CatDocument =(ProductDocument) CatApplication.ActiveDocument;
+            CatDocument = (ProductDocument)CatApplication.ActiveDocument;
+            // 添加一个新零件
+            string Name = "NewPoint";
+            Part PartID;
+            try
+            {
+                PartID=((PartDocument)CatApplication.Documents.Item(Name + ".CATPart")).Part;
+            }
+            catch (Exception)
+            {
+               CatDocument.Product.Products.AddNewComponent("Part", Name);
+               PartID = ((PartDocument)CatApplication.Documents.Item(Name + ".CATPart")).Part;
 
+            }
+            Selection SelectArc = CatDocument.Selection;
+            if (SelectArc.Count < 1)
+            {
+                MessageBox.Show("请先选择对象后再点此命令！");
+                return;
+            }
+            int ERR = 0;
+            for (int i = 1; i < SelectArc.Count; i++)
+            {
+
+                    HybridShapeFactory PartHyb = (HybridShapeFactory)PartID.HybridShapeFactory;
+                    Reference PointRef = SelectArc.Item(i).Reference;
+                    HybridShapePointCenter NewPoint = PartHyb.AddNewPointCenter(PointRef);
+                    NewPoint.set_Name("YPoint_" + i);
+                    HybridBodies Hybs = PartID.HybridBodies;
+                    HybridBody Hyb = Hybs.Item("几何图形集.1");
+                    Hyb.AppendHybridShape(NewPoint);
+                    PartID.InWorkObject = NewPoint;
+                try
+                {
+                    PartID.Update();
+                }
+                catch (Exception)
+                {
+                    ERR+=1;
+                }
+            }
+            if (ERR>0)
+            {
+                MessageBox.Show("共计:" + ERR + "个点创建新参考点失败！");
+            }
 
         }
 
